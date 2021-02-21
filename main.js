5bd77d2db89a4c8dbc05b4ed7bd37c3f8af478ab
Gopika <gopika@deerom.com>
2021-02-21 23:02:39 +0530

    js commit

---
+++ main.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+ $('.header').height($(window).height());
+ 
+})
+
+#scroll effect
+
+$(".navbar a").click(function(){
+  $("body,html").animate({
+   scrollTop:$("#" + $(this).data('value')).offset().top
+  },1000)
+  
+ })
+
+
